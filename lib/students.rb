4975e94cafb6809e5ec8823c2cfa467d8453c740
Brennan Roel <brennanroel@gmail.com>
2020-05-13 23:36:18 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,23 +1,19 @@
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM students;"
+  "SELECT MAX(gpa) FROM students"
 end
 
 def lowest_student_gpa
-  "SELECT MIN (students.gpa) FROM students;"
+  "SELECT MIN(gpa) FROM students"
 end
 
 def average_student_gpa
-  "SELECT AVG(students.gpa) FROM students;"
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(students.tardies) FROM students;"
+  "SELECT SUM(tardies) FROM students"
 end
 
-def counts_all_students_groups_by_grade
-  "SELECT COUNT(*) FROM students GROUP BY grade;"
-end
-
-def average_student_gpa_for_9th_grade
-  "SELECT AVG (students.gpa) FROM students WHERE grade = 9;"
+def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
