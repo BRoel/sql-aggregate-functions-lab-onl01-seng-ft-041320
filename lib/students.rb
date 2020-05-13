946d438941a57e2df0d2ba20aafba3ad931abf4c
Brennan Roel <brennanroel@gmail.com>
2020-05-13 23:34:59 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -18,7 +18,7 @@ def counts_all_students_groups_by_grade
   "SELECT COUNT(*) FROM students GROUP BY grade;"
 end
 
-def average_gpa_for_9th_grade
+
 def average_student_gpa_for_9th_grade
   "SELECT AVG (students.gpa) FROM students WHERE grade = 9;"
 end
